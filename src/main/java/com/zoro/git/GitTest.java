993ae56995d970884d40a78cae7fe1534ab780bb
chenbaolin <chenbaolin2020@163.com>
2021-06-01 22:10:09 +0800

    first commit

---
+++ src/main/java/com/zoro/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.zoro.git;
+
+/**
+ * @author CBL
+ * @date 2021/6/1 22:08
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
